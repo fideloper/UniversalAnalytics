ddf7d392cccdcec4cf6209a02e691ab89b9b9662
Chris Fidao <chris.fidao@digitalsurgeons.com>
2013-07-19 13:00:55 -0700

    Request class updated for getters/setters. Removed type tracking from request class, as provided by entities

--- src/UniversalAnalytics/Request.php
+++ src/UniversalAnalytics/Request.php
@@ -6,13 +6,119 @@ use UniversalAnalytics\Track\Entity;
 
 class Request {
 
+    /**
+     * Base URL for UA api
+     *
+     * @access protected
+     */
 	protected $base = 'https://ssl.google-analytics.com/collect';
 
+    /**
+     * Attributes commone to every request
+     * See getters/setters below
+     *
+     * @access protected
+     */
+    protected $attributes = array(
+        'v' => null,
+        'tid' => null,
+        'cid' => null,
+    );
+
+    public function __construct(Array $attributes=null)
+    {
+        if( is_null($attributes) === false )
+        {
+            $this->build($attributes);
+        }
+    }
+
+    /**
+     * Add object attributes via array
+     *
+     * @param Array     Key => Value array of attributes
+     * @return Request
+     */
+    public function build(Array $data)
+    {
+        foreach( $data as $key => $value )
+        {
+            $this->attributes[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Send request and generate response
+     *
+     * @param Entity
+     * @return Response
+     */
 	public function send(Entity $entity)
 	{
-		$browser = new Browser;
-		$response = $browser->submit($this->base, $entity->toArray(true), RequestInterface::METHOD_POST, array());
+        // Build from Entity, using google attributes
+        // This will add to the attributes array
+        $this->build($entity->toArray(true));
+
+		$buzzBrowser = new Browser;
+		$buzzResponse = $buzzBrowser->submit($this->base, $this->attributes, RequestInterface::METHOD_POST, array());
+
+        return new Response($buzzResponse);
 	}
 
+    /**
+     * Version 't' getter/setter
+     *
+     * @param String    API Version
+     * @return Request
+     */
+    public function version($version=null)
+    {
+        if( is_null($version) )
+        {
+            return $this->attributes['v'];
+        }
+
+        $this->attributes['v'] = $version;
+
+        return $this;
+    }
+
+    /**
+     * Tracking ID 'tid' getter/setter
+     *
+     * @param String    Tracking ID
+     * @return Request
+     */
+    public function trackingid($trackingid=null)
+    {
+        if( is_null($version) )
+        {
+            return $this->attributes['tid'];
+        }
+
+        $this->attributes['tid'] = $version;
+
+        return $this;
+    }
+
+    /**
+     * Client anonymous ID 'cid' getter/setter
+     *
+     * @param String    Client ID
+     * @return Request
+     */
+    public function clientid($clientid=null)
+    {
+        if( is_null($version) )
+        {
+            return $this->attributes['cid'];
+        }
+
+        $this->attributes['cid'] = $version;
+
+        return $this;
+    }
 
 }
